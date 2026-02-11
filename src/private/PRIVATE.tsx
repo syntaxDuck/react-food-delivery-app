ef331c5aa8dd4a94d25409037584fcb884ae76b8
syntaxDuck <kam.comer96@gmail.com>
2026-02-11 06:01:26 -0600

    Updated gitignore

---
+++ src/private/PRIVATE.tsx
@@ -0,0 +1,2 @@
+export const API_KEY = import.meta.env.VITE_FIREBASE_API_KEY || 'missing_firebase_api_key'
+export const PROJECT_ID = import.meta.env.VITE_FIREBASE_PROJECT_ID || 'missing_firebase_project_id'
